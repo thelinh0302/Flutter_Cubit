5fb61edff30da927ae5fc01c64e7dea1317bad77
ext-linhchau <ext-linhchau@spdigital.sg>
2022-06-29 23:28:25 +0700

    first commit

---
+++ lib/welcome_page.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'misc/colors.dart';
+import 'widgets/app_large_text.dart';
+import 'widgets/app_text.dart';
+import 'widgets/responsive_button.dart';
+
+class WelcomePage extends StatefulWidget {
+  const WelcomePage({Key? key}) : super(key: key);
+
+  @override
+  State<WelcomePage> createState() => _WelcomePageState();
+}
+
+class _WelcomePageState extends State<WelcomePage> {
+  List images = ["welcome-one.png", "welcome-two.png", "welcome-three.png"];
+  List text = [
+    "Mountain",
+    "River",
+    "Beach",
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return (Scaffold(
+      body: PageView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: images.length,
+          itemBuilder: (_, index) {
+            return Container(
+              width: double.infinity,
+              height: double.infinity,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage("assets/images/" + images[index]),
+                      fit: BoxFit.cover)),
+              child: Container(
+                margin: const EdgeInsets.only(top: 150, left: 20, right: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        AppLargeText(text: "Trips"),
+                        AppText(text: text[index], size: 30),
+                        const SizedBox(height: 20),
+                        Container(
+                            width: 250,
+                            child: AppText(
+                              text:
+                                  "Mountain hikes give you an incredible sense of freedom along with endurance tests",
+                              color: AppColors.textColor2,
+                              size: 14,
+                            )),
+                        const SizedBox(height: 40),
+                        ResponsiveButton()
+                      ],
+                    ),
+                    Column(
+                      children: List.generate(3, (indexSlider) {
+                        return Container(
+                          margin:  const EdgeInsets.only(bottom: 2),
+                          width: 8,
+                          height: index == indexSlider ? 25 : 8,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            color: index == indexSlider? AppColors.mainColor : AppColors.mainColor.withOpacity(0.3)
+                          ),
+                        );
+                      }),
+                    )
+                  ],
+                ),
+              ),
+            );
+          }),
+    ));
+  }
+}
---
+++ lib/widgets/responsive_button.dart
@@ -0,0 +1,29 @@
+import 'package:first_app/misc/colors.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ResponsiveButton extends StatelessWidget {
+  bool? isResponsive;
+  double? width;
+
+  ResponsiveButton({Key? key, this.width, this.isResponsive = false})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: 60,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: AppColors.mainColor
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+        Image.asset("assets/images/button-one.png"),
+      ]),
+    );
+  }
+}
